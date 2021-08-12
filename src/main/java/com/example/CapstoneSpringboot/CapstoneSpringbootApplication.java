41ed4de2473655bbba8983ea2b464e147443fc19
nathanmurfy <nathanmurphyy02@gmail.com>
2021-08-12 15:46:40 +1000

    Initial commit

---
+++ src/main/java/com/example/CapstoneSpringboot/CapstoneSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.CapstoneSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CapstoneSpringboot/CapstoneSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CapstoneSpringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
